7ebc1772fe0e8e2a970da4e26c1f444a03bb5a35
olivernan <nanyiyang@gmail.com>
2020-07-05 16:47:45 +0800

    update

--- build/precache-manifest.df74cfdd8919d963ba05aa128bd10197.js
+++ build/precache-manifest.df74cfdd8919d963ba05aa128bd10197.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "389553ee5cd5924689d5f1804f22e23f",
+    "revision": "173458d2fa92264bf8519406b1509fda",
     "url": "/resume/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/resume/static/js/2.44e4ee60.chunk.js"
   },
   {
-    "revision": "dae66d0b17278e794716",
-    "url": "/resume/static/js/main.a755d75b.chunk.js"
+    "revision": "40850a7b4ca3995679d3",
+    "url": "/resume/static/js/main.f54b88ec.chunk.js"
   },
   {
     "revision": "699f89d957187d0ac949",
