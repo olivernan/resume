0a7c168a2627227987bb84afe7163216b6bd9f81
olivernan <nanyiyang@gmail.com>
2020-07-05 15:57:09 +0800

    Updates

---
+++ precache-manifest.048b3c82630aa9bc322475a183d58f08.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc5fd51b555edeac7ec761d5ab706628",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "a1d5413aa2a4d91f0e40",
+    "url": "/resume/static/js/2.44e4ee60.chunk.js"
+  },
+  {
+    "revision": "a826e77699246733cede",
+    "url": "/resume/static/js/main.2326b101.chunk.js"
+  },
+  {
+    "revision": "699f89d957187d0ac949",
+    "url": "/resume/static/js/runtime~main.30522d0f.js"
+  }
+]);
